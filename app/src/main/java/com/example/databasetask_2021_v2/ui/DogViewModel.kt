727208d5cdf8efdae3bb8592548b589b82d355d8
alexsit19 <alexsit12@gmail.com>
2021-09-20 01:10:44 +0300

    clear DogViewModel from garbage

--- app/src/main/java/com/example/databasetask_2021_v2/ui/DogViewModel.kt
+++ app/src/main/java/com/example/databasetask_2021_v2/ui/DogViewModel.kt
@@ -1,6 +1,5 @@
 package com.example.databasetask_2021_v2.ui
 
-import android.util.Log
 import androidx.lifecycle.*
 import com.example.databasetask_2021_v2.repository.DogRepository
 import com.example.databasetask_2021_v2.repository.room.Dog
@@ -17,20 +16,7 @@ class DogViewModel(private var _repository: DogRepository): ViewModel() {
     }
     val dogs get() = _dogs
 
-    init {
-//        val list = listOf(Dog(name="a", age=1, breed ="a"), Dog(name="b", age=2, breed="b"))
-//        _dogs.value = list
-
-    }
-
-
-
-
-
     fun getAllSortBy(sortBy: String) {
-        val list = listOf(Dog(name="c", age=3, breed ="c"), Dog(name="d", age=4, breed="d"), Dog(0, "g", 4, "5"))
-
-
         this.sortbyFlow.value = sortBy
 
     }
@@ -38,7 +24,7 @@ class DogViewModel(private var _repository: DogRepository): ViewModel() {
     fun insert(name: String, age: Int, breed: String){
         val dog = Dog(0, name, age, breed)
         viewModelScope.launch { repository.save(dog) }
-        //getAll()
+
     }
 
     fun delete(dog: Dog) {
@@ -52,7 +38,6 @@ class DogViewModel(private var _repository: DogRepository): ViewModel() {
     fun getDataSortedBy(sortBy: String) {
         getAllSortBy(sortBy)
         this.sortbyFlow.value = sortBy
-        //getAll(sortBy)
 
     }
 
